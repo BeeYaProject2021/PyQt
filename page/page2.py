4ed91b21dc7b6a47f07e7bc76fb0dfabc35d9201
skeletonlin <skeleton@gmail.com>
2021-09-29 10:17:20 +0800

    0929 add

--- page/page2.py
+++ page/page2.py
@@ -37,6 +37,7 @@ class Lablemove(QLabel):
         if e.button() == Qt.LeftButton:
             print('press')
 
+#main page
 class initialWidget(QtWidgets.QMainWindow):
     
     print("initialWidget")
@@ -45,29 +46,54 @@ class initialWidget(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow2()
         self.ui.setupUi(self)
 
-        self.chosepage = initialWidget1()
+        self.chosepage = initialWidget1() # chosepage is other page
 
         self.ui.nextstep.clicked.connect(self.nextstepClicked)
-        self.ui.actionNew.triggered.connect(self.chosepage.show)
+        self.ui.actionNew.triggered.connect(self.chosepage.show) # isCheck action button show chosepage
+        self.ui.conv2D.clicked.connect(self.action_conv2D)
+        self.ui.maxpooling2D.clicked.connect(self.action_maxpooling2D)
+        self.ui.flatten.clicked.connect(self.action_flatten)
+        self.ui.dense.clicked.connect(self.action_dense)
         self.initUI()
     
     def initUI(self):
         self.setAcceptDrops(True)    
-        self.conv2D = Lablemove('conv2D', self)
-        self.conv2D.move(54, 122)
-        self.maxpooling2D = Lablemove('maxpooling2D', self)
-        self.maxpooling2D.move(54, 152)
-        self.flatten = Lablemove('flatten', self)
-        self.flatten.move(54, 182)
-        self.dense = Lablemove('dense', self)
-        self.dense.move(54, 212)
+        self.moveconv2D = Lablemove('conv2D', self)
+        self.moveconv2D.hide()
+        self.movemaxpooling2D = Lablemove('maxpooling2D', self)
+        self.movemaxpooling2D.hide()
+        self.moveflatten = Lablemove('flatten', self)
+        self.moveflatten.hide()
+        self.movedense = Lablemove('dense', self)
+        self.movedense.hide()
 
     def nextstepClicked(self):
         print("nextstep is clicked.")
+
+    def action_conv2D(self, e):
+        print("conv2D")
+        # self.moveconv2D.move(50, 400)
+        self.moveconv2D.show()
+        self.moveconv2D.move(50, 120)
+
+    def action_maxpooling2D(self, e):
+        print("maxpooling2D")
+        self.movemaxpooling2D.show()
+        self.movemaxpooling2D.move(30, 190)
+    
+    def action_flatten(self, e):
+        print("flatten")
+        self.moveflatten.show()
+        self.moveflatten.move(53, 260)
+    
+    def action_dense(self, e):
+        print("dense")
+        self.movedense.show()
+        self.movedense.move(55, 330)
     
     def dragEnterEvent(self, e):
         e.accept()
-
+    
     def dragMoveEvent(self, e):
         # self.conv2D.move(e.pos() - self.conv2D.epos)
         # e.accept()
@@ -95,25 +121,25 @@ class initialWidget(QtWidgets.QMainWindow):
         e.source().move(e.pos() - e.source().epos)
         e.setDropAction(Qt.MoveAction)
         e.accept()
+    
 
 
         
-
+#chosepage
 class initialWidget1(QtWidgets.QMainWindow):
     print("initialWidget1")
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ui = Ui_MainWindow1()
         self.ui.setupUi(self)
-        # self.show()
         self.ui.CNN.hide()
         self.ui.DQN.hide()
         self.ui.GAN.hide()
         self.ui.tool1.clicked.connect(self.tool1Clicked)
         self.ui.tool2.clicked.connect(self.tool2Clicked)
         self.ui.tool3.clicked.connect(self.tool3Clicked)
-        # self.ui.pushButton.clicked.connect(QCoreApplication.instance().quit)
-        self.ui.pushButton.clicked.connect(self.close)
+        # self.ui.pushButton.clicked.connect(QCoreApplication.instance().quit) #close all page
+        self.ui.pushButton.clicked.connect(self.close) #only colse now page
     def tool1Clicked(self):
         print("CNN is clicked.")
         self.ui.DQN.hide()
