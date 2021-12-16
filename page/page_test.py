6d33e52c034a2c378e9c9a61b48f3b9d76dc914e
NickyHaHa <nicky17345@gmail.com>
2021-12-16 21:00:47 +0800

    single image prediction complete

--- page/page_test.py
+++ page/page_test.py
@@ -77,8 +77,58 @@ class Thread(QThread):
             print("guess count: ", g_cnt)
 
 class Thread2(QThread):
-    def __init__(self):
+
+    error_signal2 = pyqtSignal(str)
+    response_signal2 = pyqtSignal(int, float)
+
+    def __init__(self, uid, h, w, c, folder_path):
         super(Thread2, self).__init__()
+        self.uid = uid
+        self.img_h = h
+        self.img_w = w
+        self.img_c = c
+        self.path = folder_path
+
+    def run(self):
+        uid = self.uid
+        h = self.img_h
+        w = self.img_w
+        c = self.img_c
+        path = self.path
+
+        url = 'http://140.136.204.132:8000/test/'
+        x = requests.post(url, data={'uid': uid, 'img_h': h, 'img_w': w, 'img_c': c}, 
+        files={'file': open(path, 'rb')})
+
+        data = re.split(" |\"", x.text)
+        print("response data", data)
+
+        ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_OOBINLINE, 1)
+        host = '140.136.204.132'
+        port = (int)(data[2])
+
+        print('Waiting for backend connection')
+        connected = False
+        while not connected:
+            try:
+                ClientSocket.connect((host, port))
+                connected = True
+            except Exception as e:
+                pass  # Do nothing, just try again
+
+        Response = ClientSocket.recv(1024)
+        strRes = Response.decode('utf-8')
+        data = strRes.split('#')
+
+        if 'error' in strRes:
+            self.error_signal2.emit(strRes)
+        else:
+            guess, probability = data[1], data[2]
+            print(guess, probability)
+            self.response_signal2.emit(int(guess), float(probability))
+            ClientSocket.close()
+
 
 class TimgWidget(QWidget):
     def __init__(self, *args, **kwargs):
@@ -116,7 +166,7 @@ class Tbatch(QWidget):
 
         self.vlayout = QVBoxLayout()
         self.vlayout.addStretch()
-        self.batchlabel = QLabel("Batch size : ")
+        self.batchlabel = QLabel("Batch size: ")
         self.batchlabel.setFont(QFont("Consolas", 15))
         self.vlayout.addWidget(self.batchlabel)
         self.batchBox = QSpinBox()
@@ -124,7 +174,7 @@ class Tbatch(QWidget):
         self.batchBox.setFixedWidth(150)
         self.vlayout.addWidget(self.batchBox)
 
-        self.imgshow = QLabel("Your Image : ")
+        self.imgshow = QLabel("Your Image: ")
         self.imgshow.setFont(QFont("Consolas", 15))
         self.imgshow.setVisible(False)
         self.vlayout.addWidget(self.imgshow)
@@ -135,6 +185,16 @@ class Tbatch(QWidget):
         self.imglabel.setScaledContents(True)
         self.imglabel.setVisible(False)
         self.vlayout.addWidget(self.imglabel)
+
+        self.guesslabel = QLabel("Predict: ")
+        self.guesslabel.setFont(QFont("Consolas", 15))
+        self.guesslabel.setVisible(False)
+        self.vlayout.addWidget(self.guesslabel)
+
+        self.guesspro = QLabel("Possibility: ")
+        self.guesspro.setFont(QFont("Consolas", 15))
+        self.guesspro.setVisible(False)
+        self.vlayout.addWidget(self.guesspro)
         
         self.vlayout.addStretch()
         self.setLayout(self.vlayout)
@@ -249,10 +309,20 @@ class TestWidget(QWidget):
         self.TIM.ChooseFileBtn.setVisible(self.TIM.ChooseFileBtn.isVisible()^1)
         self.TB.batchlabel.setVisible(self.TB.batchlabel.isVisible()^1)
         self.TB.batchBox.setVisible(self.TB.batchBox.isVisible()^1)
+        self.TT.lossLabel.setVisible(self.TT.lossLabel.isVisible()^1)
+        self.TT.accLabel.setVisible(self.TT.accLabel.isVisible()^1)
+
+        self.TT.lossLabel.setText("Test Loss: ")
+        self.TT.accLabel.setText("Test Acc: ")
 
         self.TB.imgshow.setVisible(self.TB.imgshow.isVisible()^1)
         self.TIM.ChooseImgBtn.setVisible(self.TIM.ChooseImgBtn.isVisible()^1)
         self.TB.imglabel.setVisible(self.TB.imglabel.isVisible()^1)
+        self.TB.guesslabel.setVisible(self.TB.guesslabel.isVisible()^1)
+        self.TB.guesspro.setVisible(self.TB.guesspro.isVisible()^1)
+
+        self.TB.guesslabel.setText("Predict: ")
+        self.TB.guesspro.setText("Probability: ")
         
     def runTest(self):
         if self.now_file == True:
@@ -261,14 +331,29 @@ class TestWidget(QWidget):
             self.thread.error_signal.connect(self.error_show)
             self.thread.guess_signal.connect(self.update_guess)
             self.thread.start()
+        else:
+            dataset = page_model.default_data
+            if dataset == 0:
+                self.thread = Thread2(page_training.uid, page_input.imgH, page_input.imgW, page_input.imgC, self.TIM.filePathEdit.text())
+            elif dataset == 1 or dataset == 2:
+                self.thread = Thread2(page_training.uid, 28, 28, 0, self.TIM.filePathEdit.text())
+            else:
+                self.thread = Thread2(page_training.uid, 32, 32, 1, self.TIM.filePathEdit.text())
+            self.thread.error_signal2.connect(self.error_show)
+            self.thread.response_signal2.connect(self.prediction)
+            self.thread.start()
 
     def updateLabel(self, loss, acc):
         self.TT.lossLabel.setText("Test Loss: " + str(loss))
-        self.TT.accLabel.setText("Test Acc: " + str(acc * 100) + " %")
+        self.TT.accLabel.setText(f'Test Acc: {(acc * 100):.2f} %')
     
     def update_guess(self, g):
         self.guess.append(g)
-    
+
+    def prediction(self, guess, pro):
+        self.TB.guesslabel.setText("Predict: " + self.class_names[guess])
+        self.TB.guesspro.setText("Possibility: " + str(pro * 100) + " %")
+
     def error_show(self, msg):
         self.warning.setText(msg)
         self.warning.setIcon(QMessageBox.Icon.Warning)
